3c584c2597aa6e9e7510a8c48b7cba89fff4284e
Ben Bruene <bb680@cam.ac.uk>
2024-10-25 13:20:46 +0100

    Added stl library extension and created files for the mapping code

---
+++ src/mapping.cpp
@@ -0,0 +1,5 @@
+#include "robot.h"
+#include <Arduino.h>
+#include <Arduino_LSM6DS3.h>
+#include "DFRobot_VL53L0X.h"
+
---
+++ src/mapping.h
@@ -0,0 +1,13 @@
+#ifndef MAPPING_H
+#define MAPPING_H
+#include "ArxContainer.h"
+
+class Mapping
+{
+    public:
+    std::map<String, int> mp {{"one", 1}, {"two", 2}};
+    private:
+
+};
+
+#endif // MAPPING_H
