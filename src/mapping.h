8e09435f49024e76c61d64dcc552ef2f69271598
Avelyn <ajb363@cam.ac.uk>
2024-10-31 14:09:29 +0000

    Nicer formatting for mapping.h

--- src/mapping.h
+++ src/mapping.h
@@ -34,18 +34,20 @@ public:
 
 private:
   ::std::map<::std::pair<Node, Node>, ::std::vector<Direction>> DirectionMap 
-  { 
-    { {Start, Factory1}, {Forward, Forward, Left, Forward, Right, Forward, Right, Forward, Right}},
-    //{ {Start, Factory2}, {Forward, Left, Forward,Right, Right, Forward, Forward, Left, Left}},
-    { {Factory1, Factory2}, {Left, Left, Right}},
-    { {Factory1, ContaminationSite}, {Right, Right, Forward, Forward}},
-    { {Factory1, Box1Right}, {Right, Right, Right}},
-    { {Factory1, Box1Left}, {Left, Left, Left}},
-    { {Factory1, Box2Right}, {Right, Right, Forward, Right}},
-    { {Factory1, Box3Right}, {Left, Left, Right}},
-    { {Factory1, Box3Left}, {Left, Forward, Left, Left}},
-    { {Factory1, Box4Middle}, {Left, Forward, Left, Forward, Left, Forward}},
-    { {Box4Middle, Start}, {Right, Forward}}
+  {
+//  | StartNode | EndNode            | Move 1 | Move 2 | Move 3 | Move 4 | Move 5 | Move 6 | Move 7 | Move 8 | Move 9 | ...
+//  | --------- | ------------------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ...
+    {{Start,      Factory1         }, {Forward, Forward, Left,    Forward, Right,   Forward, Right,   Forward, Right}},
+    {{Start,      Factory2         }, {Forward, Left,    Forward, Right,   Right,   Forward, Forward, Left,    Left}},
+    {{Factory1,   Box4Middle       }, {Left,    Forward, Left,    Forward, Left,    Forward}},
+    {{Factory1,   ContaminationSite}, {Right,   Right,   Forward, Forward}},
+    {{Factory1,   Box2Right        }, {Right,   Right,   Forward, Right}},
+    {{Factory1,   Box3Left         }, {Left,    Forward, Left,    Left}},
+    {{Factory1,   Factory2         }, {Left,    Left,    Right}},
+    {{Factory1,   Box1Right        }, {Right,   Right,   Right}},
+    {{Factory1,   Box1Left         }, {Left,    Left,    Left}},
+    {{Factory1,   Box3Right        }, {Left,    Left,    Right}},
+    {{Box4Middle, Start            }, {Right,   Forward}}
   };
 };
 
